71e9769f00798d9e0d8a9ef63c5b21cc8852c281
Hadi Raza <hadiraza.edu@gmail.com>
2023-02-02 23:56:33 +0500

    object_practice

---
+++ app.js
@@ -0,0 +1,10 @@
+let _name = "Hadi";
+let _class = "Matric";
+
+let nameObj = {
+    _name: _name,  //This is old way
+}
+let nameobj1 = {
+    _name,         //This is new way
+    _class         //This is new way
+}
